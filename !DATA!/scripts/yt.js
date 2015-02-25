af3b7e0abde1ec4e6ae288cb20635931ad038e13
literallybenjam <bjmns@me.com>
2015-02-24 19:51:40 -0800

    try dis insted

--- !DATA!/scripts/yt.js
+++ !DATA!/scripts/yt.js
@@ -15,7 +15,7 @@ function onYouTubeIframeAPIReady() {
     var i;
     yt.frames = document.querySelectorAll('iframe[src^="https://www.youtube.com/embed/"], iframe[src^="http://www.youtube.com/embed/"], iframe[src^="https://youtube.com/embed/"], iframe[src^="http://youtube.com/embed/"]');
     for (i = 0; i < yt.frames.length; i++) {
-        yt.frames.item(i).yt_player = new YT.Player(yt.frames.item(i), {events: {'onReady': onready}});
+        yt.frames.item(i).yt_player = new YT.Player(yt.frames.item(i), {events: {'onStateChange': onready}});
         yt.frames.item(i).addEventListener("scrolledTo", yt.processScroll, false);
     }
 }
