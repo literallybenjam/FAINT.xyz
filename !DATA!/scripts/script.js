b84cbe5672c8c03e194ba59903ec61340eea6c48
literallybenjam <bjmns@me.com>
2015-02-27 15:51:58 -0800

    relative url cssget fix

--- !DATA!/scripts/script.js
+++ !DATA!/scripts/script.js
@@ -70,9 +70,8 @@ function readStyleSheet(ss) {
     var s = "";
     if (!ss.cssRules) return;
     for (i = 0; i < ss.cssRules.length; i++) {
-        console.log(ss.cssRules.item(i).cssText);
         if (ss.cssRules.item(i).type != CSSRule.IMPORT_RULE) s += ss.cssRules.item(i).cssText;
-        else if (ss.cssRules.item(i).href.substr(0, window.location.origin) === window.location.origin) s += readStyleSheet(ss.cssRules.item(i).styleSheet);
+        else if (ss.cssRules.item(i).href.indexOf(":") === -1 || ss.cssRules.item(i).href.substr(0, window.location.origin) === window.location.origin) s += readStyleSheet(ss.cssRules.item(i).styleSheet);
     }
     return s;
 }
