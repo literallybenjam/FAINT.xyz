3b95c1a08eb42ea383a3b65da7b2a0a1c1d97878
literallybenjam <bjmns@me.com>
2015-03-09 18:32:54 -0700

    export-js; scriptloader

--- !DATA!/scripts/script.js
+++ !DATA!/scripts/script.js
@@ -1,5 +1,6 @@
 /* jslint asi:true, browser:true */
 /* global CSSRule */
+/* global Export */
 
 var current_scroll_source = null;
 var current_scroll_element = null;
@@ -7,6 +8,12 @@ var current_scroll_target = 0;
 var current_scroll_velocity = 0;
 var current_scroll_hash = "#";
 var should_push_state = false;
+
+var scripts = {
+    "export-js": 1
+}
+var scripts_loaded = 0;
+var MAX_SCRIPTS = 1;
 
 function scroll() {
     var max_scroll = window.scrollMaxY;
@@ -65,205 +72,6 @@ function checkLinks() {
     }
 }
 
-function readStyleSheet(ss) {
-    var i;
-    var styles = "";
-    var imports = "";
-    if (!ss.cssRules) return;
-    for (i = 0; i < ss.cssRules.length; i++) {
-        if (ss.cssRules.item(i).type === CSSRule.IMPORT_RULE) {
-            if (ss.cssRules.item(i).href.indexOf(":") === -1 || ss.cssRules.item(i).href.substr(0, window.location.origin) === window.location.origin) {
-                styles += readStyleSheet(ss.cssRules.item(i).styleSheet).styles;
-                imports += readStyleSheet(ss.cssRules.item(i).styleSheet).imports;
-            }
-            else imports += ss.cssRules.item(i).cssText;
-        }
-        else styles += ss.cssRules.item(i).cssText;
-    }
-    return {styles: styles, imports: imports};
-}
-
-function getHTML() {
-    var i;
-    var s = "<!DOCTYPE html>\n<!--  " + document.title + ", originally located at " + window.location.href + '  -->\n<html lang="' + document.documentElement.lang + '">\n    <head>\n';
-    for (i = 0; i < document.head.children.length; i++) {
-        switch (document.head.children.item(i).tagName) {
-            case "LINK":
-                if (document.head.children.item(i).type == "text/css") s += "        <style>" + readStyleSheet(document.head.children.item(i).sheet).imports + readStyleSheet(document.head.children.item(i).sheet).styles + "</style>\n";
-                break;
-
-            case "SCRIPT":
-                s += "        <!--  script omitted  -->\n";
-                break;
-
-            default:
-                s += "        " + document.head.children.item(i).outerHTML + "\n";
-                break;
-        }
-    }
-    s += "    </head>\n    " + document.body.outerHTML + "\n</html>";
-    return s;
-}
-
-function exportNode(node) {
-    var s = "";
-    var i;
-    switch (node.nodeType) {
-
-        case Node.TEXT_NODE:
-            s = node.textContent.replace(/\s+/, " ");
-            break;
-
-        case Node.ELEMENT_NODE:
-            if (node.classList.contains("export-link") || node.classList.contains("export-hidden")) break;
-            for (i = 0; i < node.childNodes.length; i++) {
-                s += exportNode(node.childNodes.item(i))
-            }
-            switch (node.tagName) {
-
-                case "A":
-                    s += " [" + node.href + "]";
-                    break;
-
-                case "ASIDE":
-                    s = "\n[[" + s + "]]\n";
-                    break;
-
-                case "B":
-                    s = "#" + s.replace(/\s+/g, "-");
-                    break;
-
-                case "BLOCKQUOTE":
-                    s = ("\n" + s.replace(/\s+$/, "").replace(/^\s+/, "")).replace(/\s*\n\s*/g, "\n").replace(/\n/g, "\n > ");
-                    break;
-
-                case "BR":
-                    s = "\n";
-                    break;
-
-                case "CODE":
-                    s = "'" + s.trim() + "'";
-                    break;
-
-                case "DEL":
-                    s = "[deleted]";
-                    break;
-
-                case "DFN":
-                    s = "_" + s.trim() + "_";
-                    break;
-
-                case "DT":
-                    s = "\n" + s + ": ";
-                    break;
-
-                case "EM":
-                    s = "~" + s.trim() + "~";
-                    break;
-
-                case "FIGCAPTION":
-                    s = "\n– " + s + "\n";
-                    break;
-
-                case "FIGURE":
-                    s = "\n" + s + "\n";
-                    break;
-
-                case "H1":
-                    s = "\n" + s.toLocaleUpperCase() + "\n";
-                    break;
-
-                case "H2":
-                    s = "\n~ " + s + " ~\n";
-                    break;
-
-                case "H3":
-                    s = "\n> " + s + ":\n";
-                    break;
-
-                case "H4":
-                    s = "\n>> " + s + ":\n";
-                    break;
-
-                case "H5":
-                    s = "\n>>> " + s + ":\n";
-                    break;
-
-                case "H6":
-                    s = "\n>>>> " + s + ":\n";
-                    break;
-
-                case "HR":
-                    s = "\n* * *\n";
-                    break;
-
-                case "I":
-                    s = "'" + s.trim() + "'";
-                    break;
-
-                case "IFRAME":
-                    s = "\n[" + node.src + "]\n";
-                    break;
-
-                case "IMG":
-                    if (node.alt) s = "[image, " + node.alt + "]";
-                    else s = "[image]";
-                    break;
-
-                case "INS":
-                    s = "_" + s.replace(/\s+/g, "_") + "_";
-                    break;
-
-                case "LI":
-                    if (node.parentElement.tagName === "OL") {
-                        for (i = 0; i < node.parentElement.children.length; i++) {
-                            if (node.parentElement.children.item(i) === node) break;
-                        }
-                        s = "\n" + (i + 1) + ". " + s + "\n";
-                    }
-                    else s = "\n· " + s + "\n";
-                    break;
-
-                case "P":
-                    if (window.getComputedStyle(node, "::before").getPropertyValue("content") == '"¶"') s = "\n¶ " + s + "\n";
-                    else if (window.getComputedStyle(node, "::after").getPropertyValue("content") == "no-close-quote") s = "\n“" + s + "\n";
-                    else if (window.getComputedStyle(node, "::after").getPropertyValue("content") == "close-quote") s = "\n“" + s + "”\n";
-                    else s = "\n" + s + "\n";
-                    break;
-
-                case "Q":
-                    s = "“" + s + "”";
-                    break;
-
-                case "SMALL":
-                    s = "((" + s.trim() + "))";
-                    break;
-
-                case "S":
-                    s = "-" + s.replace(/\s+/, "-") + "-";
-                    break;
-
-                case "STRONG":
-                    s = s.toLocaleUpperCase();
-                    break;
-
-                case "SUB":
-                    s = "_[" + s.trim() + "]";
-                    break;
-
-                case "SUP":
-                    s = "^[" + s.trim() + "]";
-                    break;
-            }
-            break;
-
-        default:
-            break;
-
-    }
-    return s.replace(/ +/g, " ").replace(/ *\n */g, "\n").replace(/\n+/g, "\n");
-}
-
 function navWalk(walker) {
     var s = "<ol>";
     do {
@@ -301,26 +109,39 @@ function navInit() {
     }
 }
 
-function exportInit() {
-    if (document.documentElement.dataset.noDownload !== undefined || !document.getElementsByTagName("MAIN")) return;
-    var plaintext = exportNode(document.getElementsByTagName("MAIN").item(0)).trim()
-    var html = getHTML();
+function init() {
+
+    checkLinks();
+
+    //  export-js
     var footer = document.createElement("FOOTER");
-    footer.innerHTML = 'download: <a href="data:text/plain;charset=utf-8,' + encodeURIComponent(plaintext) + '" target="_blank">plain text</a> / <a href="data:text/html;charset=utf-8,' + encodeURIComponent(html) + '" target="_blank">html</a>';
+    Export.init(footer);
     document.getElementsByTagName("MAIN").item(0).appendChild(footer);
     document.styleSheets.item(0).insertRule("@media print{main > footer:last-child {display: none;}}", document.styleSheets.item(0).cssRules.length);
-}
 
-function init() {
-    checkLinks();
-    exportInit();
     navInit();
+
     var hashLinks = document.querySelectorAll('a[href^="#"]');
     for (var i = 0; i < hashLinks.length; i++) {
         hashLinks.item(i).addEventListener("click", navHashFromLink, false);
     }
+
     navHashFromLocation();
+
 }
 
-document.addEventListener("DOMContentLoaded", init, false);
+function scriptLoaded(e) {
+    scripts_loaded &= scripts[this.dataset.name];
+    if (scripts_loaded & MAX_SCRIPTS === MAX_SCRIPTS) init();
+}
+
+function loadScripts() {
+    var tag = document.createElement('script');
+    tag.dataset.name = "export-js";
+    tag.src = "/!DATA!/scripts/export-js/export.js";
+    tag.addEventListener("loaded", scriptLoaded, false);
+    document.head.appendChild(tag, document.head.firstElementChild);
+}
+
+document.addEventListener("DOMContentLoaded", loadScripts, false);
 window.addEventListener("popstate", navHashFromLocation, false);
